bd02554574f068c12acc0bd49229e241a5fa2952
ikamgaing5 <ikamgaing5@gmail.com>
2024-07-30 07:44:02 +0100

    init

--- git.sql
+++ git.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 26 juil. 2024 à 23:17
+-- Généré le : mar. 30 juil. 2024 à 06:42
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -51,8 +51,8 @@ CREATE TABLE IF NOT EXISTS `personne` (
 INSERT INTO `personne` (`id`, `nom`, `prenom`, `nomfam`, `mdp`, `code`, `sexe`, `datenaiss`, `lieunaiss`, `numtel`, `idpar`, `etat`) VALUES
 (1, 'Du Mont', 'Henri', 'Du Mont', 'henri', 'henri', 'M', '1962-07-10', 'Douala', '656544543', 0, 1),
 (5, 'Larson', 'Nicky', 'Larson', 'larson', 'nicky', 'M', '2002-10-23', 'Douala', '656544542', 1, 1),
-(6, 'Jordan', 'Michel', NULL, NULL, 'michel', 'err', '1991-07-10', 'Londres', '656544556', 1, 1),
-(7, 'Harris', 'Calvin', NULL, NULL, 'calvin', 'err', '2007-02-28', 'Moscou', '698767879', 1, 0),
+(6, 'Jordan', 'Michel', NULL, NULL, 'michel', 'M', '1991-07-10', 'Londres', '656544556', 1, 1),
+(7, 'Harris', 'Calvin', NULL, NULL, 'calvin', 'M', '2007-02-28', 'Moscou', '698767879', 1, 0),
 (8, 'Giveon', 'Roy', NULL, NULL, 'roy', 'M', '2001-06-13', 'Berlin', '656677665', 1, 1),
 (9, 'Marverick', 'Mat', NULL, NULL, 'mat', 'M', '2004-02-04', 'Caire', '656544345', 5, 1);
 COMMIT;
